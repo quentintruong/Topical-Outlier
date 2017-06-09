173ac6fa41612a696638f704d42942a1d40c6401
quentintruong <quentintruong@users.noreply.github.com>
2017-06-09 04:01:43 -0700

    output

--- topical_outlier.py
+++ topical_outlier.py
@@ -1,12 +1,12 @@
 import gensim
 
-#Load Google's pretrained word2vec model
-print("Start loading Google's pretrained word2vec model, this may take a while")
+# Load Google's pretrained word2vec model
+print("Loading Google's pretrained word2vec model, this may take a while")
 model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
 print("Finished loading")
 
-#Find outlier in set using gensim and word2vec
+# Find outlier in set using gensim and word2vec
 mySet = "red green blue orange white black sausage purple pink"
 mySet = mySet.split()
-print("Find topical outlier in", mySet)
+print("Finding topical outlier in, this may take a while", mySet)
 print("Topical outlier: ", model.doesnt_match(mySet))
