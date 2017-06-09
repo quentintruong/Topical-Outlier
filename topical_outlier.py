0dab8bca6995fdcb4a486c9f5a2f523cca9b951b
quentintruong <quentintruong@users.noreply.github.com>
2017-06-09 03:52:26 -0700

    comm

--- topical_outlier.py
+++ topical_outlier.py
@@ -1,14 +1,3 @@
-# Quentin Truong
-# Solution to "The Phamtom Intern Challenge: #3" 
-#
-# Challenge Statement:
-# 3. Given a set of ten words identify a topical outlier. For example in the following set, sausage is an outlier: “red green blue orange white black sausage purple pink”
-#
-# Requires: 
-# python3.5 
-# gensim
-# a pretrained word2vec model (For example: https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit)
-
 import gensim
 
 #Load Google's pretrained word2vec model
